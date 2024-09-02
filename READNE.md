10d3ec2c51ed9fcf7828eed3738a2a3a0f6e6447
zxkkkk <858654746@qq.com>
2024-09-02 16:07:58 +0800

    quickstart模块创建完成

---
+++ READNE.md
@@ -0,0 +1,3 @@
+## 这是 Spring boot 的课程学习多模块项目
+### 模块说明
+- spring-boot-quickstart:快速起步模块
---
+++ spring-boot-quickstart/src/main/java/top/zxk/springboot/quickstart/Application.java
@@ -0,0 +1,12 @@
+package top.zxk.springboot.quickstart;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.SpringApplication;
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
